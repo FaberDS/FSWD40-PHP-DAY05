fd3921c670b75596b83ec7ab96788916323c3616
Denis Schuele <schueledenis@gmx.at>
2018-06-25 13:57:38 +0200

    a

--- Prework/print_extend/index.php
+++ Prework/print_extend/index.php
@@ -5,9 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 </head>
 <body>
-    <table>
+    <form action="server-side.php" method="POST" id="myForm">
+        <div class="form-group">
+            <input type="text" name="name" placeholder="name">
+
+        </div>
+        <div class="form-group">
+            <input type="text" name="position" placeholder="position">
+            
+        </div>
+        <div class="form-group">
+            <input type="text" name="salary" placeholder="salary">
+            
+        </div>
+        <div class="form-group">
+            <Button type="button" id="submit" value="senden">senden</Button>
+            
+        </div>
+            
+            
+
+
+
+        </form>
+    <table class="table">
 
         <tr>
             <th>First Name</th>
@@ -55,5 +79,7 @@
         }
     
     </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js" integrity="sha384-u/bQvRA/1bobcXlcEYpsEdFVK/vJs3+T+nXLsBYJthmdBuavHvAW6UsmqO2Gd/F9" crossorigin="anonymous"></script>
 </body>
 </html>
