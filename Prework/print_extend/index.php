4701d879e43d53d067cc446351fecb32568ae28a
Denis Schuele <schueledenis@gmx.at>
2018-06-25 13:58:21 +0200

    pr

--- Prework/print_extend/index.php
+++ Prework/print_extend/index.php
@@ -31,6 +31,16 @@
 
 
         </form>
+        <form>
+            <div class="row">
+                <div class="col">
+                <input type="text" class="form-control" placeholder="First name">
+                </div>
+                <div class="col">
+                <input type="text" class="form-control" placeholder="Last name">
+                </div>
+            </div>
+        </form>
     <table class="table">
 
         <tr>
